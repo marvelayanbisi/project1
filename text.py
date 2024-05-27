05a0998adc9b430a6af26af48c5585b050310f24
adebisienoch <root@kali.local>
2024-05-27 14:43:33 +0100

    first project

---
+++ text.py
@@ -0,0 +1,20 @@
+#/USR/bin/python3
+ # Ask the user for their first name
+first_name = input("Enter your first name: ")
+
+    # Ask the user for their last name
+last_name = input("Enter your last name: ")
+
+    # Ask the user for their age
+age = input("Enter your age: ")
+    
+    # Print out the collected information
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Age:", age)
+
+  
+
+ 
+
+
